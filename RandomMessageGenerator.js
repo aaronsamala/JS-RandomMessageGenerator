5960ae470aa781072099465238f8d3d50b6bf656
aaronsamala <aaronsamala@gmail.com>
2023-06-07 02:34:06 -1000

    Created the messages and updated the Project Notes

--- RandomMessageGenerator.js
+++ RandomMessageGenerator.js
@@ -9,3 +9,21 @@
 // Plan: Create a function that will tell the user how their day might go, what their spirit animal is, and their lucky lotto numbers.
 // The lotto numbers will follow the Powerball format that shows up on Google: 5 numbers 1-69, and 1 number 1-26.
 
+
+// dayOutlook function returns the random outlook for the day as a string.
+const dayOutlook = () => {
+    const dayPossibilities = ["very bad", "bad", "neither bad nor well", "well", "very well"]
+
+}
+
+
+// spiritAnimal function returns the random spirit animal as a string.
+const spiritAnimal = () => {
+    const animalPossibilities = ["the wolf", "the bat", "the bear", "the dog", "the pig", "the cow", "the chicken", "the eagle", "the owl", "THE CHUCK NORRIS"]
+}
+
+
+// lottoNumbers function returns the lotto numbers as a string.
+const lottoNumbers = () => {
+    const returnString = "Your lotto numbers are " + "."
+}
