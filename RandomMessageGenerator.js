9ba7c58751daa3a94d730fce6db03bb28d597c84
aaronsamala <aaronsamala@gmail.com>
2023-06-07 01:37:21 -1000

    AKS-Initial-JS-Updated-Plan

--- RandomMessageGenerator.js
+++ RandomMessageGenerator.js
@@ -6,6 +6,6 @@
 
 // Prompt Summary: Create a program that generates a random message for the user.
 
-// Plan: Create an array that will tell the user how their day might go, what their spirit animal is, and their lucky lotto numbers.
+// Plan: Create a function that will tell the user how their day might go, what their spirit animal is, and their lucky lotto numbers.
 // The lotto numbers will follow the Powerball format that shows up on Google: 5 numbers 1-69, and 1 number 1-26.
 
